41923361ea76a900946d54a1bbe37781acd3bab0
Saul Ordonez <sauleordonez@gmail.com>
2020-01-31 14:25:49 -0800

    update create methods

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,6 @@
 class Cat < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :sex, presence: true
+  validates :bio, presence: true
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,7 @@
 class Dog < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :sex, presence: true
+  validates :size, presence: true
+  validates :bio, presence: true
 end
